9cec9cc88a3092d3f24ec14da8d32150848a7159
Nils Hartmann <nils@nilshartmann.net>
2025-02-21 13:15:05 +0100

    Simplified ArticleListNavBar

--- src/components/articlelistpage/ArticleListNavBar.tsx
+++ src/components/articlelistpage/ArticleListNavBar.tsx
@@ -1,15 +1,12 @@
 import { OrderButton } from "@/components/articlelistpage/OrderButton";
 import ButtonBar from "@/components/ButtonBar";
-import { NavButtonBar } from "@/components/NavButtonBar";
 
 export default function ArticleListNavBar() {
   return (
-    <NavButtonBar align={"right"}>
-      <ButtonBar>
-        <OrderButton orderBy={undefined}>Date</OrderButton>
-        <OrderButton orderBy={"LIKES"}>Likes</OrderButton>
-        <OrderButton orderBy={"CATEGORY"}>Category</OrderButton>
-      </ButtonBar>
-    </NavButtonBar>
+    <ButtonBar align={"end"}>
+      <OrderButton orderBy={undefined}>Date</OrderButton>
+      <OrderButton orderBy={"LIKES"}>Likes</OrderButton>
+      <OrderButton orderBy={"CATEGORY"}>Category</OrderButton>
+    </ButtonBar>
   );
 }
