fda6b7f054e87f14860ad56ea61914ece32b972e
Nils Hartmann <nils@nilshartmann.net>
2025-02-22 08:34:34 +0100

    RelatedArticleBox: Stories

---
+++ src/components/articlepage/RelatedArticleBox.stories.tsx
@@ -0,0 +1,27 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import RelatedArticleBox from "@/components/articlepage/RelatedArticleBox";
+
+const meta: Meta<typeof RelatedArticleBox> = {
+  component: RelatedArticleBox,
+  args: {
+    onNextClick() {},
+    onPrevClick() {},
+    article: {
+      id: "A_8",
+      title: "Advancements in Wind Energy Technology",
+      image: {
+        uri: "/images/articles/s_8.webp",
+        altText: "Alt text for Advancements in Wind Energy Technology",
+      },
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof RelatedArticleBox>;
+
+export const Primary: Story = {
+  args: {},
+};
